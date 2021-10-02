4bedc23190c8e2cdb5a5cacc458fe1f1dc568f93
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-10-02 21:48:46 +0800

    intial structure

---
+++ flutter_payment_history/android/app/src/main/kotlin/com/example/flutter_payment_history/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_payment_history
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_payment_history/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_payment_history/presentation/screens/Payment_history_screen.dart';
+
+import 'app.dart';
+
+void main() {
+  runApp(MyApp());
+}
