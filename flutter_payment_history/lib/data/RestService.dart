969826577959bb5151122bac01f6fb5fe47b0482
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-10-03 08:38:02 +0800

    added rest service

---
+++ flutter_payment_history/lib/data/RestService.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RestService {
+  static final RestService _instance = RestService._constructor();
+  factory RestService() {
+    return _instance;
+  }
+
+  RestService._constructor();
+
+  Uri baseUrl(String endPoint) =>
+      Uri.parse("http://10.0.2.2:5001/votiship/us-central1/app/$endPoint");
+
+  Future get(String endpoint) async {
+    final response = await http.get(baseUrl(endpoint));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    throw response;
+  }
+
+  Future post(String endpoint, {dynamic data}) async {
+    final response = await http.post(baseUrl(endpoint),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(data));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+
+    throw jsonDecode(response.statusCode.toString());
+  }
+
+  Future patch(String endpoint, {dynamic data}) async {
+    final response = await http.patch(baseUrl(endpoint),
+        headers: {'Content-Type': 'application/json'}, body: jsonEncode(data));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    throw response;
+  }
+
+  Future delete(String endpoint) async {
+    final response = await http.delete(baseUrl(endpoint));
+
+    if (response.statusCode == 200) {
+      return;
+    }
+    throw response;
+  }
+}
