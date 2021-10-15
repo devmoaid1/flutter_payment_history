1774a5f09dd980a04c1b39965153b7ba95858eab
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-10-15 21:24:30 +0800

    slider

--- flutter_payment_history/lib/presentation/screens/topup_screen/topup_screen.dart
+++ flutter_payment_history/lib/presentation/screens/topup_screen/topup_screen.dart
@@ -4,8 +4,75 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:iconly/iconly.dart';
 
 class TopUpScreen extends StatelessWidget {
-  const TopUpScreen({Key key}) : super(key: key);
+  String value = "FPX";
+  TopUpScreen({Key key}) : super(key: key);
 
+  final List<Widget> amounts = [
+    Container(
+      child: Text(
+        "10",
+        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
+      ),
+      decoration: BoxDecoration(shape: BoxShape.rectangle),
+      padding: EdgeInsets.all(20),
+    ),
+    Container(
+      child: Text(
+        "10",
+        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
+      ),
+      decoration: BoxDecoration(shape: BoxShape.rectangle),
+      padding: EdgeInsets.all(20),
+    ),
+    Container(
+      child: Text(
+        "10",
+        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
+      ),
+      decoration: BoxDecoration(shape: BoxShape.rectangle),
+      padding: EdgeInsets.all(20),
+    ),
+    Container(
+      child: Text(
+        "10",
+        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
+      ),
+      decoration: BoxDecoration(shape: BoxShape.rectangle),
+      padding: EdgeInsets.all(20),
+    ),
+    Container(
+      child: Text(
+        "10",
+        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
+      ),
+      decoration: BoxDecoration(shape: BoxShape.rectangle),
+      padding: EdgeInsets.all(20),
+    ),
+    Container(
+      child: Text(
+        "10",
+        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
+      ),
+      decoration: BoxDecoration(shape: BoxShape.rectangle),
+      padding: EdgeInsets.all(20),
+    ),
+    Container(
+      child: Text(
+        "10",
+        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
+      ),
+      decoration: BoxDecoration(shape: BoxShape.rectangle),
+      padding: EdgeInsets.all(20),
+    ),
+    Container(
+      child: Text(
+        "10",
+        style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600),
+      ),
+      decoration: BoxDecoration(shape: BoxShape.rectangle),
+      padding: EdgeInsets.all(20),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +90,14 @@ class TopUpScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   SizedBox(
-                    width: 10,
+                    width: 20,
                   ),
                   Flexible(
                     flex: 1,
                     child: IconButton(
                         icon: Icon(
                           IconlyLight.arrow_left_2,
-                          size: 18,
+                          size: 20,
                         ),
                         onPressed: () {
                           Navigator.pop(context);
@@ -40,7 +107,7 @@ class TopUpScreen extends StatelessWidget {
                     width: 50,
                   ),
                   Flexible(
-                    flex: 6,
+                    flex: 7,
                     child: Center(
                       child: Text(
                         "Top Up eRider Wallet",
@@ -49,9 +116,9 @@ class TopUpScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(
-                    width: 30,
-                  ),
+                  // SizedBox(
+                  //   width: 10,
+                  // ),
                   Flexible(
                     flex: 3,
                     child: TextButton(
@@ -63,8 +130,117 @@ class TopUpScreen extends StatelessWidget {
                               fontSize: 15,
                               fontWeight: FontWeight.w600),
                         )),
-                  )
+                  ),
                 ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 35,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Select Payment Method",
+                          style: GoogleFonts.poppins(
+                              fontSize: 15, fontWeight: FontWeight.w700),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          width: 330,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(10),
+                                  bottomRight: Radius.circular(10),
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10))),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "FPX",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 17, fontWeight: FontWeight.w600),
+                              ),
+                              Icon(
+                                IconlyLight.arrow_right_2,
+                                size: 20,
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        Container(
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 120,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Amount",
+                                    style:
+                                        GoogleFonts.poppins(color: Colors.grey),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "\$ 10.00",
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.w700),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: 30,
+                            ),
+                            Container(
+                              width: 300,
+                              child: Slider(
+                                min: 0,
+                                max: 5000,
+                                value: 30,
+                                onChanged: null,
+                                activeColor: appOrange,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        // GridView.count(
+                        //   crossAxisCount: 4,
+                        //   crossAxisSpacing: 4,
+                        //   mainAxisSpacing: 2,
+                        //   children: amounts,
+                        // )
+                      ],
+                    ),
+                  ],
+                ),
               )
             ],
           ),
